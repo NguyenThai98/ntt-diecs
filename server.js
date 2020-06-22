417500d2c3f27a60d19a65e49471a0252ef40217
NguyenThai <nguyenthai492017#gmail.com>
2020-06-22 13:03:07 +0700

    add

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const serveStatic = require('serve-static');
 const path = require('path');
 const app = express();
 let port = process.env.PORT || 3000
-app.use(serveStatic(__dirname + "/public"))
+app.use('/', serveStatic(path.join(__dirname, '/public')))
 
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
 app.listen(port);
 console.log('listenting on port', port);
